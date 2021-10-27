0411e4eb77e0f089aa59e0bec80e6d5d28c6bb9d
ouiam.dot <o.suilahi@gmail.com>
2021-10-27 21:37:24 +0100

    Add algorithm of least turns && Improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import {Board} from './Optimized-path-finder/Board/Board';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { connectFunctionsEmulator, getFunctions, httpsCallable } from 'firebase/functions';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOh33-NCPkemiF_SIOVvSRP-AeGs_-wxk",
+    authDomain: "optimized-path-finder-1a74f.firebaseapp.com",
+    projectId: "optimized-path-finder-1a74f",
+    storageBucket: "optimized-path-finder-1a74f.appspot.com",
+    messagingSenderId: "221335645760",
+    appId: "1:221335645760:web:ed229600239c9909a339a5",
+    measurementId: "G-1ZGCWNPC48"
+};
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app);
+
+export const pathfinding = httpsCallable(functions, 'pathfinding');
+// connectFunctionsEmulator(functions, "localhost", 5001);
 
 function App() {
   return (
